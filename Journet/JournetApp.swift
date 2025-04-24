118675ce115baed62e42e7dc186acc1b16f1eb0c
Jake Bromberg <jake.bromberg@gmail.com>
2025-04-24 13:00:15 -0700

    Initial Commit

---
+++ Journet/JournetApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournetApp.swift
+//  Journet
+//
+//  Created by Jake Bromberg on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct JournetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
